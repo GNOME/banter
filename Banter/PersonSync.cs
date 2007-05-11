d2b4bc14114cb20e0a034f640a9962107bef7d1f
Calvin Gaisford <calvinrg@gmail.com>
2007-05-11 17:37:57 +0000

    Added PersonSync.cs to project Created the person sync class. This class
    2007-05-11  Calvin Gaisford <calvinrg@gmail.com> 
    
            * Banter.mdp, Banter/Makefile.am: Added PersonSync.cs to project
            * Banter/PersonSync.cs: Created the person sync class.  This class will
              be in charge of the sync between Telepathy and EDS.
    
    
    svn path=/trunk/; revision=23

---
+++ Banter/PersonSync.cs
@@ -0,0 +1,46 @@
+//***********************************************************************
+// *  $RCSfile$ - PersonSync.cs
+// *
+// *  Copyright (C) 2007 Novell, Inc.
+// *
+// *  This program is free software; you can redistribute it and/or
+// *  modify it under the terms of the GNU General Public
+// *  License as published by the Free Software Foundation; either
+// *  version 2 of the License, or (at your option) any later version.
+// *
+// *  This program is distributed in the hope that it will be useful,
+// *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// *  General Public License for more details.
+// *
+// *  You should have received a copy of the GNU General Public
+// *  License along with this program; if not, write to the Free
+// *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// *
+// **********************************************************************
+
+
+using System;
+
+namespace Banter
+{
+	///<summary>
+	///	PersonSync Class
+	/// Synchronizes the Telepathy contacts with EDS contacts.  This is done using
+	/// the ProviderUserManager and the PersonStore.
+	///</summary>
+	public class PersonSync
+	{
+
+		#region Constructors
+		/// <summary>
+		/// Creates a new PersonSync
+		/// </summary>			
+		public PersonSync()
+		{
+		}
+		#endregion
+		
+		
+	}
+}
